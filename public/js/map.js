a83294e976c608d2b07c8212c48c0f3472d69eaa
Dinesh <dineshthiru.1998@gmail.com>
2020-01-02 00:46:53 +0530

    app complete

--- public/js/map.js
+++ public/js/map.js
@@ -5,3 +5,53 @@ style: 'mapbox://styles/mapbox/streets-v11',
 zoom: 9,
 center: [-71.157895, 42.707741]
 });
+
+// fetch stores from API
+async function getStores() {
+    const res = await fetch('/api/v1/stores');
+    const data = await res.json();
+
+    const stores  = data.data.map(store => {
+        return {
+                'type': 'Feature',
+                'geometry': {
+                'type': 'Point',
+                'coordinates': [store.location.coordinates[0], store.location.coordinates[1]]
+                },
+                'properties': {
+                    storeId: store.storeId,
+                    icon: 'shop',
+                }
+        }
+    });
+
+    loadMap(stores);
+}
+
+
+// Load map with stores
+function loadMap(stores) {
+    map.on('load', function() {
+        map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': {
+            'type': 'geojson',
+            'data': {
+            'type': 'FeatureCollection',
+            'features': stores,
+            }
+            },
+            'layout': {
+            'icon-image': '{icon}-15',
+            'icon-size': 1.5,
+            'text-field': '{storeId}',
+            'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+            'text-offset': [0, 0.9],
+            'text-anchor': 'top'
+            }
+            });
+    });
+}
+
+getStores();
